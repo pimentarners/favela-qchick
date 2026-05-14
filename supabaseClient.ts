f91d07c810ad449bc5c102855a86c1a3b5d06c1f
Diogenes Pimenta <diogenesmatos@outlook.com>
2026-05-13 22:37:43 -0300

    chore: vincula projeto ao supabase e cria script sql inicial

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -6,7 +6,7 @@ import { createClient } from '@supabase/supabase-js';
 // 2. Copie a "Project URL" e a "anon public" key
 // 3. Cole abaixo:
 
-const supabaseUrl = 'https://xgplyhtzfdkvpnqkeazi.supabase.co'; // Ex: https://abcdefghijklm.supabase.co
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhncGx5aHR6ZmRrdnBucWtlYXppIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2OTI3Nzg4NSwiZXhwIjoyMDg0ODUzODg1fQ.LE2-1Ir6XI_K2Bz2ELnZtS6_Qs14-BuIzjmfHbMP228'; // Ex: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...
+const supabaseUrl = 'https://ltnkqxfrtvnmsmllebkq.supabase.co'; 
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0bmtxeGZydHZubXNtbGxlYmtxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2OTYxMjYsImV4cCI6MjA5NDI3MjEyNn0.0mgYFZLIxjoCgD7dtE3DksKt2uGE30AvrrcEefxVggA';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
